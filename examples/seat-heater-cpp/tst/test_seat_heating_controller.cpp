043b1db06ddfd6e05dfc012401a40b6cd3c2eaac
Thabo Krick <thabo.krick@btc-embedded.com>
2026-03-09 09:36:49 +0100

    commented test for demo

--- examples/seat-heater-cpp/tst/test_seat_heating_controller.cpp
+++ examples/seat-heater-cpp/tst/test_seat_heating_controller.cpp
@@ -62,7 +62,6 @@ TEST_F(SeatHeatingControllerTestSuite, Test_CheckConditions_SeatUnoccupied) {
 // /**
 //  * @brief Verify activation conditions fail when power not OK
 //  */
-//// BTC_LINK_TO_REQ(subterra:data-service:objects:/default/seatheater${WorkItem}SEAT-79)
 // TEST_F(SeatHeatingControllerTestSuite, Test_CheckConditions_PowerNotOk) {
 //     EXPECT_FALSE(SeatHeatingController::check_conditions(true, NOK, NO_ERROR));
 // }
